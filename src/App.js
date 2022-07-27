fe2b0e1f2226efeceaccbe7b571867e38974c52c
Joey <josephdguillaume@gmail.com>
2022-07-27 10:50:21 -0400

    Added Pagination.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
 import Table from './components/Table';
 
-const columns = [
-  { name: 'Airline', property: 'airline' },
-  { name: 'Source Airport', property: 'src' },
-  { name: 'Destination Airport', property: 'dest' },
-];
-
-const App = () => (
-  <div className="app">
-    <header className="header">
-      <h1 className="title">Airline Routes</h1>
-    </header>
-    <section>
-      <p>Welcome to the app!</p>
-    </section>
-    <section>
-      <Table className="routes-table" columns={columns} rows="" format="" />
-    </section>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <p>Welcome to the app!</p>
+      </section>
+      <section>
+        <Table className="routes-table" perPage={49} />
+      </section>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
-import { routes, airlines, aiports, airports } from '../data';
+import { useEffect, useState } from 'react';
+import { airlines, airports, routes } from '../data';
 
 const getAirlineById = id => {
   return airlines.find(airline => airline.id === id).name;
@@ -8,28 +9,73 @@ const getAirportByCode = code => {
   return airports.find(airport => airport.code === code).name;
 };
 
-const Table = ({ className, columns, rows, format }) => {
+const Table = ({ className, perPage }) => {
+  const [routeSelection, setRouteSelection] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(perPage - 1);
+  const numberOfRoutes = routes.length;
+
+  useEffect(() => {
+    const routesPart = routes.slice(startIndex, endIndex + 1);
+    setRouteSelection(routesPart);
+  }, [startIndex, endIndex, perPage]);
+
+  const prevButtonStatus = startIndex === 0 ? true : false;
+  const nextButtonStatus =
+    startIndex + perPage >= numberOfRoutes ? true : false;
+
+  const handlePrev = () => {
+    setStartIndex(startIndex - perPage);
+    setEndIndex(endIndex - perPage);
+  };
+
+  const handleNext = () => {
+    setStartIndex(startIndex + perPage);
+    setEndIndex(endIndex + perPage);
+  };
+
   return (
-    <table className={className}>
-      <thead>
-        <tr>
-          <th scope="col">Airline</th>
-          <th scope="col">Source Airport</th>
-          <th scope="col">Destination Airport</th>
-        </tr>
-      </thead>
-      <tbody>
-        {routes.map(route => {
-          return (
-            <tr>
-              <td>{getAirlineById(route.airline)}</td>
-              <td>{getAirportByCode(route.src)}</td>
-              <td>{getAirportByCode(route.dest)}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className={className}>
+        <thead>
+          <tr>
+            <th scope="col">Airline</th>
+            <th scope="col">Source Airport</th>
+            <th scope="col">Destination Airport</th>
+          </tr>
+        </thead>
+        <tbody>
+          {routeSelection.map((route, index) => {
+            return (
+              <tr key={index}>
+                <td>{getAirlineById(route.airline)}</td>
+                <td>{getAirportByCode(route.src)}</td>
+                <td>{getAirportByCode(route.dest)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <p>
+        Showing {startIndex === 0 ? startIndex + 1 : startIndex}-
+        {endIndex <= numberOfRoutes ? endIndex + 1 : numberOfRoutes} of{' '}
+        {numberOfRoutes} routes.
+      </p>
+      <button
+        id="prev-page"
+        disabled={prevButtonStatus}
+        onClick={() => handlePrev()}
+      >
+        Prev Page
+      </button>
+      <button
+        id="next-page"
+        disabled={nextButtonStatus}
+        onClick={() => handleNext()}
+      >
+        Next Page
+      </button>
+    </>
   );
 };
 
